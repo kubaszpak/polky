9c2d954fd99a9151a966efa03a43e1da9357cbfb
kubaszpak <jakub.szpak.it@gmail.com>
2022-07-03 01:31:36 +0200

    Added logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,12 +38,21 @@ const apartments = [
 const Home: NextPage = () => {
 	return (
 		<>
-			<Box className="relative top-0 left-0 w-full h-52" />
-			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 max-w-7xl mx-auto gap-5 p-10 ">
+			<Box className="flex w-full p-10 justify-between">
+				<div className="max-w-[240px]">
+					<Image width={586} height={154} src="/logo.png" alt="Domky" />
+				</div>
+				<div>
+					<ul className="flex gap-5">
+						<li>Home</li>
+						<li>About</li>
+					</ul>
+				</div>
+			</Box>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto gap-5 p-10 ">
 				{apartments.map((apartment, idx) => {
 					return (
 						<div key={idx} className="p-5 ">
-							<h1>{apartment.name}</h1>
 							<div className="relative min-h-[20rem]">
 								<Image
 									src="/assets/apartment1.jpg"
@@ -53,6 +62,9 @@ const Home: NextPage = () => {
 									className="rounded-md"
 								/>
 							</div>
+							<h1>{apartment.name}</h1>
+							<h2>{apartment.price}</h2>
+							<h2>{apartment.people}</h2>
 						</div>
 					);
 				})}
